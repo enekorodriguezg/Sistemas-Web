2a5dd9078423efa8f8ade1f456d788b8c1d4c9d9
enekorodriguezg <enekorodriguezgarcia@gmail.com>
2026-02-02 13:50:55 +0100

    TO DO: arreglar check_channel y send_petition

--- Prácticas/Práctica1/Practica1_ER.py
+++ Prácticas/Práctica1/Practica1_ER.py
@@ -1,4 +1,5 @@
 #-*- coding: UTF-8 -*-
+from asyncio.windows_events import NULL
 
 import requests,psutil,time,signal,sys,urllib
 
@@ -20,10 +21,32 @@ def cpu_ram():
     print("CPU: %" + str(cpu) + "\tRAM: %" + str(ram))
     time.sleep(5)
 
-def send_petition(method, uri, headers, cuerpo,allow_red):
-    return requests.request(method,uri,headers=headers,data=cuerpo, allow_redirects=allow_red)
+def send_petition(method, uri, headers=None, cuerpo=None, params=None, allow_red=False):
+    return requests.request(method, uri, headers=headers, data=cuerpo, params=params, allow_redirects=allow_red)
+
+def check_channel():
+    method= 'GET'
+    uri= 'https://api.thingspeak.com/channels.json'
+    headers= {'Host': 'api.thingspeak.com',
+              'Content-Type': 'application/x-www-form-urlencoded'}
+    cuerpo={'api_key': API_KEY,
+            'tag': 'ergCanal'}
+
+
+    response= send_petition(method, uri, cuerpo)
+    print(response.content)
+
+    if response.content:
+        return True
+    else:
+        return False
 
 def create_channel():
+
+    if check_channel():
+        print('[!] El canal ya existe por lo que no se creará\r\n[!] Saliendo...\n')
+        sys.exit(0)
+
     global channel_id
     global write_key
 
@@ -40,17 +63,18 @@ def create_channel():
               'field1': '%CPU',
               'field2': '%RAM',
               'public_flag': 'true'}
-
+    params=''
     cuerpo_encoded= urllib.parse.urlencode(cuerpo)
     #print(cuerpo_encoded)
     headers['Content-Length'] = str(len(cuerpo_encoded))
-    response = send_petition(method, uri, headers, cuerpo_encoded, False)
+    response = send_petition(method, uri, headers=headers, cuerpo=cuerpo_encoded)
     codigo= response.status_code
     #print(codigo)
     if codigo==200:
         print('[+] Canal creado correctamente. Compruébalo en ThingSpeak')
     else:
         print(f"[-] Error al crear canal. Código de estado {codigo}")
+        sys.exit(0)
 
     data=response.json()
     channel_id=data.get('id')
@@ -58,10 +82,11 @@ def create_channel():
 
 
     if channel_id!=None and write_key!=None:
-        file=open("channel_id_and_write_api_key.txt", "w")
-        file.write(str(channel_id))
-        file.write(write_key)
+        file=open("id_and_key.txt", "w")
+        file.write("ID: " + str(channel_id))
+        file.write("\nWrite key: " + write_key)
         file.close()
+        print("[+] ID del canal y API de escritura copiadas al archivo 'id_and_key.txt' correctamente")
 
 
 
