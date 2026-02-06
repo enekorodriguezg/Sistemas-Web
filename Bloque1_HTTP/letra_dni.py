78f47f7141bad63c7a19df04e02ac9576dc82f41
enekorodriguezg <enekorodriguezgarcia@gmail.com>
2026-02-06 12:33:17 +0100

    letra_dni v1

---
+++ Bloque1_HTTP/letra_dni.py
@@ -0,0 +1,24 @@
+#-*- coding: UTF-8 -*-
+import sys
+import urllib
+from pydoc import describe
+
+import requests
+
+method='POST'
+uri='https://ws-sendata.appspot.com/dni/letra'
+cabeceras={'Host': 'ws-sendata.appspot.com',
+           'Content-Type': 'application/x-www-form-urlencoded'}
+cuerpo={'dni':sys.argv[1]}
+
+cuerpo_encoded=urllib.parse.urlencode(cuerpo)
+
+cabeceras['Content-Lenght'] = str(len(cuerpo_encoded))
+
+respuesta=requests.request(method,uri,data=cuerpo_encoded,headers=cabeceras,allow_redirects=False)
+
+codigo=respuesta.status_code
+descripcion=respuesta.reason
+
+print(str(codigo)+' '+descripcion)
+
