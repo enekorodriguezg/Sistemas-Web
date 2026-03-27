0fccd6659b02709846a454130c5df860c3730f97
enekorodriguezg <enekorodriguezgarcia@gmail.com>
2026-03-27 11:17:05 +0100

    shareinfo

---
+++ ShareInfo/src/main/java/shareinfo/LoginServlet.java
@@ -0,0 +1,69 @@
+package shareinfo;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import helper.db.*;
+
+public class LoginServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	private MySQLdb mySQLdb;
+	
+	public void init(ServletConfig config) {
+		System.out.println("---> Entrando en init()de LoginServlet");
+		mySQLdb = new MySQLdb();
+		System.out.println("<--- Saliendo de init() de LoginServlet");
+	}
+ 
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+    	System.out.println("---> Entrando en doPost() de LoginServlet");
+    	
+    	request.setCharacterEncoding("UTF-8");
+    	
+    	String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		System.out.println("Estrayendo parámetros de la petición: " + email + " " + password);
+
+		String username = mySQLdb.getUsername(email, password);
+		
+		if(username == null) {
+			System.out.println("Login error: redireccionando al usuarioa a loginForm.html");
+			RequestDispatcher rd = request.getRequestDispatcher("/html/loginForm.html");
+			rd.forward(request, response);
+		} else {
+			HttpSession session = request.getSession(true);
+			
+			String sessionID = session.getId();
+			System.out.println("Sesion de Usuario para " + username + ": " + sessionID);
+			System.out.println("Cogiendo la lista de usuarios activos de contexto: ");
+			ServletContext context = request.getServletContext();
+			HashMap<String, String> loggedinUsers = (HashMap) context.getAttribute("loggedin_users");
+			if(loggedinUsers == null) {
+				System.out.println("Lista vacia");
+				loggedinUsers = new HashMap();
+				loggedinUsers.put(username, sessionID);
+			} else {
+				if(!loggedinUsers.containsKey(username)) {
+					System.out.println(username + " no esta en la lista");
+					loggedinUsers.put(username, sessionID);
+				} else {
+					System.out.println(username + " ya esta en la lista");
+				}
+			}
+			context.setAttribute("loggedin_users", loggedinUsers);
+			System.out.println("Loggedin users: " + loggedinUsers);
+			
+			System.out.println("----Redireccionar al usuario al MainServlet");
+			RequestDispatcher rd = context.getNamedDispatcher("MainServlet");
+			rd.forward(request, response);
+		}
+		
+		System.out.println("<--- Saliendo de doPost() en LoginServlet");
+    }
+}
+
---
+++ ShareInfo/src/main/java/shareinfo/MySessionListener.java
@@ -0,0 +1,43 @@
+package shareinfo;
+
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+ 
+public class MySessionListener implements HttpSessionListener {
+
+	
+	public MySessionListener() {
+	        System.out.println("Listener registrado");
+	    }
+
+ 
+	public void sessionCreated(HttpSessionEvent event) {
+		System.out.println("----Una sesion esta siendo creada");
+	}
+ 
+	public void sessionDestroyed(HttpSessionEvent event) {
+		System.out.println("----Una sesion esta siendo destruida");
+		
+		HttpSession session = event.getSession();
+		
+		String sessionID = session.getId();
+		System.out.println(" Cogiendo sessionID del usuario: " + sessionID);
+		ServletContext context = session.getServletContext();
+		HashMap<String, String> loggedinUsers = (HashMap) context.getAttribute("loggedin_users");
+		System.out.println(" Loggedin users: " + loggedinUsers.toString());
+		
+		for(Map.Entry<String, String> entry : loggedinUsers.entrySet()) {
+            if(entry.getValue().equals(sessionID)) {
+            	loggedinUsers.remove(entry.getKey());
+            	System.out.println("Eliminando " + entry.getKey() + " de loggedin_users");
+            	context.setAttribute("loggedin_users", loggedinUsers);
+            	System.out.println("Loggedin users: " + loggedinUsers);
+            	break;
+            }
+		}
+	}
+ 
+}
+
+ 
